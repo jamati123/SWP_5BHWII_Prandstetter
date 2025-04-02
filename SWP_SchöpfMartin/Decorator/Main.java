b02652db7eca68c1264694bb8f2ef500fd17285f
jamati123 <fourtyeightcrash@gmail.com>
2025-04-02 11:19:11 +0200

    Fix

--- SWP_SchöpfMartin/Decorator/Main.java
+++ SWP_SchöpfMartin/Decorator/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         kaffee simpleKaffee = new implementsKaffee();
-        System.out.println(simpleKaffee.decorate() + " Price: " + simpleKaffee.price());
+        System.out.println(simpleKaffee.decorate() + " Preis: " + simpleKaffee.price());
 
         kaffee zuckerKaffee = new zucker(simpleKaffee);
-        System.out.println(zuckerKaffee.decorate() + " Price: " + zuckerKaffee.price());
+        System.out.println(zuckerKaffee.decorate() + " Preis: " + zuckerKaffee.price());
 
         kaffee milchKaffee = new milch(simpleKaffee);
-        System.out.println(milchKaffee.decorate() + " Price: " + milchKaffee.price());
+        System.out.println(milchKaffee.decorate() + " Preis: " + milchKaffee.price());
 
         kaffee milchZucker = new milch(zuckerKaffee);
-        System.out.println(milchZucker.decorate() + " Price: " + milchZucker.price());
+        System.out.println(milchZucker.decorate() + " Preis: " + milchZucker.price());
 
         kaffee milchZuckerSuess = new suesstoff(milchZucker);
-        System.out.println(milchZuckerSuess.decorate() + " Price: " + milchZuckerSuess.price());
+        System.out.println(milchZuckerSuess.decorate() + " Preis: " + milchZuckerSuess.price());
 
 
         
